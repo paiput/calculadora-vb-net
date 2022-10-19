05bb0106b506ab68fadd00c7e045280df6bd716e
Lucas Piputto <lucaspiputto@gmail.com>
2022-10-19 16:14:51 -0300

    Primer commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,266 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BC = New System.Windows.Forms.Button()
+        Me.B1 = New System.Windows.Forms.Button()
+        Me.B2 = New System.Windows.Forms.Button()
+        Me.B3 = New System.Windows.Forms.Button()
+        Me.B4 = New System.Windows.Forms.Button()
+        Me.B5 = New System.Windows.Forms.Button()
+        Me.B6 = New System.Windows.Forms.Button()
+        Me.B9 = New System.Windows.Forms.Button()
+        Me.B7 = New System.Windows.Forms.Button()
+        Me.B8 = New System.Windows.Forms.Button()
+        Me.B0 = New System.Windows.Forms.Button()
+        Me.BDel = New System.Windows.Forms.Button()
+        Me.BSub = New System.Windows.Forms.Button()
+        Me.BEq = New System.Windows.Forms.Button()
+        Me.BSum = New System.Windows.Forms.Button()
+        Me.Display = New System.Windows.Forms.TextBox()
+        Me.Display2 = New System.Windows.Forms.TextBox()
+        Me.BMul = New System.Windows.Forms.Button()
+        Me.BDiv = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'BC
+        '
+        Me.BC.Location = New System.Drawing.Point(230, 352)
+        Me.BC.Name = "BC"
+        Me.BC.Size = New System.Drawing.Size(106, 50)
+        Me.BC.TabIndex = 0
+        Me.BC.Text = "C"
+        Me.BC.UseVisualStyleBackColor = True
+        '
+        'B1
+        '
+        Me.B1.Location = New System.Drawing.Point(230, 240)
+        Me.B1.Name = "B1"
+        Me.B1.Size = New System.Drawing.Size(50, 50)
+        Me.B1.TabIndex = 1
+        Me.B1.Text = "1"
+        Me.B1.UseVisualStyleBackColor = True
+        '
+        'B2
+        '
+        Me.B2.Location = New System.Drawing.Point(286, 240)
+        Me.B2.Name = "B2"
+        Me.B2.Size = New System.Drawing.Size(50, 50)
+        Me.B2.TabIndex = 2
+        Me.B2.Text = "2"
+        Me.B2.UseVisualStyleBackColor = True
+        '
+        'B3
+        '
+        Me.B3.Location = New System.Drawing.Point(342, 240)
+        Me.B3.Name = "B3"
+        Me.B3.Size = New System.Drawing.Size(50, 50)
+        Me.B3.TabIndex = 3
+        Me.B3.Text = "3"
+        Me.B3.UseVisualStyleBackColor = True
+        '
+        'B4
+        '
+        Me.B4.Location = New System.Drawing.Point(230, 184)
+        Me.B4.Name = "B4"
+        Me.B4.Size = New System.Drawing.Size(50, 50)
+        Me.B4.TabIndex = 4
+        Me.B4.Text = "4"
+        Me.B4.UseVisualStyleBackColor = True
+        '
+        'B5
+        '
+        Me.B5.Location = New System.Drawing.Point(286, 184)
+        Me.B5.Name = "B5"
+        Me.B5.Size = New System.Drawing.Size(50, 50)
+        Me.B5.TabIndex = 5
+        Me.B5.Text = "5"
+        Me.B5.UseVisualStyleBackColor = True
+        '
+        'B6
+        '
+        Me.B6.Location = New System.Drawing.Point(342, 184)
+        Me.B6.Name = "B6"
+        Me.B6.Size = New System.Drawing.Size(50, 50)
+        Me.B6.TabIndex = 6
+        Me.B6.Text = "6"
+        Me.B6.UseVisualStyleBackColor = True
+        '
+        'B9
+        '
+        Me.B9.Location = New System.Drawing.Point(342, 128)
+        Me.B9.Name = "B9"
+        Me.B9.Size = New System.Drawing.Size(50, 50)
+        Me.B9.TabIndex = 7
+        Me.B9.Text = "9"
+        Me.B9.UseVisualStyleBackColor = True
+        '
+        'B7
+        '
+        Me.B7.Location = New System.Drawing.Point(230, 128)
+        Me.B7.Name = "B7"
+        Me.B7.Size = New System.Drawing.Size(50, 50)
+        Me.B7.TabIndex = 7
+        Me.B7.Text = "7"
+        Me.B7.UseVisualStyleBackColor = True
+        '
+        'B8
+        '
+        Me.B8.Location = New System.Drawing.Point(286, 128)
+        Me.B8.Name = "B8"
+        Me.B8.Size = New System.Drawing.Size(50, 50)
+        Me.B8.TabIndex = 8
+        Me.B8.Text = "8"
+        Me.B8.UseVisualStyleBackColor = True
+        '
+        'B0
+        '
+        Me.B0.Location = New System.Drawing.Point(230, 296)
+        Me.B0.Name = "B0"
+        Me.B0.Size = New System.Drawing.Size(106, 50)
+        Me.B0.TabIndex = 9
+        Me.B0.Text = "0"
+        Me.B0.UseVisualStyleBackColor = True
+        '
+        'BDel
+        '
+        Me.BDel.Location = New System.Drawing.Point(398, 128)
+        Me.BDel.Name = "BDel"
+        Me.BDel.Size = New System.Drawing.Size(50, 50)
+        Me.BDel.TabIndex = 10
+        Me.BDel.Text = "CE"
+        Me.BDel.UseVisualStyleBackColor = True
+        '
+        'BSub
+        '
+        Me.BSub.Location = New System.Drawing.Point(398, 240)
+        Me.BSub.Name = "BSub"
+        Me.BSub.Size = New System.Drawing.Size(50, 50)
+        Me.BSub.TabIndex = 11
+        Me.BSub.Text = "-"
+        Me.BSub.UseVisualStyleBackColor = True
+        '
+        'BEq
+        '
+        Me.BEq.Location = New System.Drawing.Point(342, 352)
+        Me.BEq.Name = "BEq"
+        Me.BEq.Size = New System.Drawing.Size(106, 50)
+        Me.BEq.TabIndex = 12
+        Me.BEq.Text = "="
+        Me.BEq.UseVisualStyleBackColor = True
+        '
+        'BSum
+        '
+        Me.BSum.Location = New System.Drawing.Point(398, 184)
+        Me.BSum.Name = "BSum"
+        Me.BSum.Size = New System.Drawing.Size(50, 50)
+        Me.BSum.TabIndex = 13
+        Me.BSum.Text = "+"
+        Me.BSum.UseVisualStyleBackColor = True
+        '
+        'Display
+        '
+        Me.Display.Enabled = False
+        Me.Display.Font = New System.Drawing.Font("Consolas", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Display.Location = New System.Drawing.Point(230, 78)
+        Me.Display.Name = "Display"
+        Me.Display.Size = New System.Drawing.Size(218, 34)
+        Me.Display.TabIndex = 14
+        '
+        'Display2
+        '
+        Me.Display2.Enabled = False
+        Me.Display2.Location = New System.Drawing.Point(230, 50)
+        Me.Display2.Name = "Display2"
+        Me.Display2.Size = New System.Drawing.Size(218, 22)
+        Me.Display2.TabIndex = 15
+        '
+        'BMul
+        '
+        Me.BMul.Location = New System.Drawing.Point(398, 296)
+        Me.BMul.Name = "BMul"
+        Me.BMul.Size = New System.Drawing.Size(50, 50)
+        Me.BMul.TabIndex = 16
+        Me.BMul.Text = "x"
+        Me.BMul.UseVisualStyleBackColor = True
+        '
+        'BDiv
+        '
+        Me.BDiv.Location = New System.Drawing.Point(342, 296)
+        Me.BDiv.Name = "BDiv"
+        Me.BDiv.Size = New System.Drawing.Size(50, 50)
+        Me.BDiv.TabIndex = 17
+        Me.BDiv.Text = "/"
+        Me.BDiv.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(674, 450)
+        Me.Controls.Add(Me.BDiv)
+        Me.Controls.Add(Me.BMul)
+        Me.Controls.Add(Me.Display2)
+        Me.Controls.Add(Me.Display)
+        Me.Controls.Add(Me.BSum)
+        Me.Controls.Add(Me.BEq)
+        Me.Controls.Add(Me.BSub)
+        Me.Controls.Add(Me.BDel)
+        Me.Controls.Add(Me.B0)
+        Me.Controls.Add(Me.B8)
+        Me.Controls.Add(Me.B7)
+        Me.Controls.Add(Me.B9)
+        Me.Controls.Add(Me.B6)
+        Me.Controls.Add(Me.B5)
+        Me.Controls.Add(Me.B4)
+        Me.Controls.Add(Me.B3)
+        Me.Controls.Add(Me.B2)
+        Me.Controls.Add(Me.B1)
+        Me.Controls.Add(Me.BC)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BC As Button
+    Friend WithEvents B1 As Button
+    Friend WithEvents B2 As Button
+    Friend WithEvents B3 As Button
+    Friend WithEvents B4 As Button
+    Friend WithEvents B5 As Button
+    Friend WithEvents B6 As Button
+    Friend WithEvents B9 As Button
+    Friend WithEvents B7 As Button
+    Friend WithEvents B8 As Button
+    Friend WithEvents B0 As Button
+    Friend WithEvents BDel As Button
+    Friend WithEvents BSub As Button
+    Friend WithEvents BEq As Button
+    Friend WithEvents BSum As Button
+    Friend WithEvents Display As TextBox
+    Friend WithEvents Display2 As TextBox
+    Friend WithEvents BMul As Button
+    Friend WithEvents BDiv As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,118 @@
+﻿Public Class Form1
+    Dim N1 As Integer
+    Dim N2 As Integer
+    Dim Mem As Boolean = False
+    Dim Op As String
+    Dim Res As Integer
+
+    Private Sub SetDisplay(ByVal p_Op As String)
+        If Display.Text = "" Then
+            N1 = 0
+        Else
+            N1 = Integer.Parse(Display.Text)
+        End If
+        Op = p_Op
+        Display2.Text = N1.ToString() + " " + Op
+        Display.Text = ""
+        Mem = False
+    End Sub
+
+    Private Sub CalcResult()
+        If Not Mem Then
+            N2 = Integer.Parse(Display.Text)
+        End If
+        Display2.Text = N1.ToString() + " " + Op + " " + N2.ToString() + " = "
+        Select Case Op
+            Case "+"
+                Res = N1 + N2
+            Case "-"
+                Res = N1 - N2
+            Case "x"
+                Res = N1 * N2
+            Case "/"
+                Res = N1 / N2
+            Case Else
+                Res = N1
+        End Select
+        Display.Text = Res.ToString()
+        Dim Aux As Integer = N1
+        N1 = Res
+        Mem = True
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub B1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles B1.Click
+        Display.Text += "1"
+    End Sub
+
+    Private Sub B2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles B2.Click
+        Display.Text += "2"
+    End Sub
+
+    Private Sub B3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles B3.Click
+        Display.Text += "3"
+    End Sub
+
+    Private Sub B4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles B4.Click
+        Display.Text += "4"
+    End Sub
+
+    Private Sub B5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles B5.Click
+        Display.Text += "5"
+    End Sub
+
+    Private Sub B6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles B6.Click
+        Display.Text += "6"
+    End Sub
+
+    Private Sub B7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles B7.Click
+        Display.Text += "7"
+    End Sub
+
+    Private Sub B8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles B8.Click
+        Display.Text += "8"
+    End Sub
+
+    Private Sub B9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles B9.Click
+        Display.Text += "9"
+    End Sub
+
+    Private Sub B0_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles B0.Click
+        Display.Text += "0"
+    End Sub
+
+    Private Sub BDel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BDel.Click
+        If Not Mem And Display.Text.Length > 0 Then
+            Display.Text = Display.Text.Substring(0, Display.Text.Length - 1)
+        End If
+    End Sub
+
+    Private Sub BC_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BC.Click
+        Display.Text = ""
+        Display2.Text = ""
+        Mem = False
+    End Sub
+
+    Private Sub BSum_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BSum.Click
+        SetDisplay("+")
+    End Sub
+
+    Private Sub BSub_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BSub.Click
+        SetDisplay("-")
+    End Sub
+
+    Private Sub BMul_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BMul.Click
+        SetDisplay("x")
+    End Sub
+
+    Private Sub BDiv_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BDiv.Click
+        SetDisplay("/")
+    End Sub
+
+    Private Sub BEq_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BEq.Click
+        CalcResult()
+    End Sub
+End Class
